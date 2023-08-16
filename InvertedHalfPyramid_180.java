7dc3b68e047434c46aed649d2e2a847cd4c487b3
Abhinav Sharma <104672596+AbhinavSharma74@users.noreply.github.com>
2023-08-16 04:37:22 -0500

    Add files via upload

---
+++ InvertedHalfPyramid_180.java
@@ -0,0 +1,16 @@
+public class InvertedHalfPyramid_180 {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i=0; i<=n; i++){
+            //Printing Spaces
+            for (int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //Printing Stars
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
